1c76cab84d74efb7e1b7be77aa38f00d8f11d9bc
Pavel Vasilchenko <pavel.s.vasilchenko@gmail.com>
2011-01-20 15:48:32 +0200

    - modify this sample so as to run multiple services with different endpoints, to Roman and Alexander can test a random selection of endpoints.

--- locator-sample/src/main/java/org/talend/esb/locator/sample/Server.java
+++ locator-sample/src/main/java/org/talend/esb/locator/sample/Server.java
@@ -1,6 +1,8 @@
 package org.talend.esb.locator.sample;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.ws.Endpoint;
 
@@ -18,7 +20,7 @@ public class Server {
         System.out.println("Starting Server on port " + serverPort + "...");
 
 //        String address = String.format("http://localhost:%1$/services/Greeter", serverPort);
-        String address = "http://localhost:" + serverPort + "/services/Greeter";
+        String address = "http://192.168.40.15:" + serverPort + "/services/Greeter";
 
         initForLocator(locatorEndpoints);
         publishService(address);
@@ -29,7 +31,6 @@ public class Server {
 	private void publishService(String address) {
 		Object implementor = new org.talend.esb.sample.cxf.GreeterImpl();
         Endpoint endpoint = Endpoint.create(implementor);
-
         endpoint.publish(address);
 	}
 
@@ -49,19 +50,33 @@ public class Server {
 	}
 
     public static void main(String args[]) throws Exception {
-    	String locatorEndpoints = "localhost:2181";
-		String serverPort = "8081";
+    	String locatorEndpoints = "192.168.40.14:2181";
+		List<String> serverPorts = new ArrayList<String>();
+		serverPorts.add("8080");
+		serverPorts.add("8081");
+		serverPorts.add("8082");
+		serverPorts.add("8083");
+		serverPorts.add("8084");
+		serverPorts.add("8085");
+		serverPorts.add("8086");
+		serverPorts.add("8087");
+		serverPorts.add("8088");
+		serverPorts.add("8089");
     	int i = 0;
-		while(i < args.length) {
+    	if(args.length > 0) serverPorts.clear();
+    	while(i < args.length) {
 			if(args[i].equals("-l")) {
 				locatorEndpoints = args[i + 1];
 				i += 2;
 			} else if(args[i].equals("-p")) {
-				serverPort = args[i + 1];
+				serverPorts.add(args[i + 1]);
 				i += 2;
 			}
+		}		
+		for(String serverPort: serverPorts) {			
+			new Server(serverPort, locatorEndpoints);
 		}
-        new Server(serverPort, locatorEndpoints);
+		
         System.out.println("Server ready...");
 
         Thread.sleep(125 * 60 * 1000);
