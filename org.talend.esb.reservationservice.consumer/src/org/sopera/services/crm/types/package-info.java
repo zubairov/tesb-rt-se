b8dc99fab9e60ec504ed76430d53037a3220894b
amarkevich <amarkevich@talend.com>
2011-01-21 12:44:16 +0200

    examples cleanup

--- org.talend.esb.reservationservice.consumer/src/org/sopera/services/crm/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://services.sopera.org/crm/types")
-package org.sopera.services.crm.types;
--- org.talend.esb.reservationservice.consumer/src/org/sopera/services/reservation/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://services.sopera.org/reservation/types")
-package org.sopera.services.reservation.types;
