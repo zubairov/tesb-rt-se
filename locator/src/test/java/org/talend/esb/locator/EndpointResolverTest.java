977de1cd11660207439c4e92cb4bf0719f80ef4c
Alexander Osadchiy <alexander.g.osadchiy@gmail.com>
2011-01-28 10:47:21 +0200

    Tests for EndpointResolver.

---
+++ locator/src/test/java/org/talend/esb/locator/EndpointResolverTest.java
@@ -0,0 +1,111 @@
+package org.talend.esb.locator;
+
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import javax.xml.namespace.QName;
+
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.Test;
+
+public class EndpointResolverTest {
+
+	public static final QName SERVICE_NAME = new QName(
+			"http://example.com/services", "service1");
+	public static final String LOCATOR_ENDPOINTS = "localhost:2181";
+
+	public static final String ENDPOINT = "http://example.com/service1";
+	public static final String ENDPOINT_1 = ENDPOINT;
+	public static final String ENDPOINT_2 = "http://example.com/service2";
+	public static final String ENDPOINT_NODE_1 = "http:%2F%2Fexample.com%2Fservice1";
+	public static final String ENDPOINT_NODE_2 = "http:%2F%2Fexample.com%2Fservice2";
+	public static final String SERVICE_PATH = ServiceLocator.LOCATOR_ROOT_PATH
+			+ "/{http:%2F%2Fexample.com%2Fservices}service1";
+	public static final String ENDPOINT_PATH = SERVICE_PATH + "/"
+			+ ENDPOINT_NODE_1;
+
+	final ZooKeeper zkMock = createMock(ZooKeeper.class);
+
+	@Test
+	public void testEndpointResolver() throws Exception {
+		EndpointResolver er = createEndpointResolver();
+		assertNotNull(er);
+	}
+
+	@Test
+	public void testEndpointResolverNullParameters() {
+		try {
+			createEndpointResolver(null, null);
+			fail("NullPointerException should be thrown");
+		} catch (NullPointerException e) {
+		}
+
+		try {
+			createEndpointResolver(SERVICE_NAME, null);
+			fail("NullPointerException should be thrown");
+		} catch (NullPointerException e) {
+		}
+
+		try {
+			createEndpointResolver(null, LOCATOR_ENDPOINTS);
+			fail("NullPointerException should be thrown");
+		} catch (NullPointerException e) {
+		}
+	}
+
+	@Test
+	public void testSelectEndpoint() {
+		EndpointResolver er = createEndpointResolver();
+		String endpoint = er.selectEndpoint();
+		assertTrue(er.getEndpointsList().contains(endpoint));
+	}
+
+	@Test
+	public void testGetEndpointsList() {
+		EndpointResolver er = createEndpointResolver();
+		assertEquals(er.getEndpointsList().get(0), ENDPOINT_1);
+		assertEquals(er.getEndpointsList().get(1), ENDPOINT_2);
+	}
+
+	@Test
+	public void testGetServiceName() {
+		EndpointResolver er = createEndpointResolver();
+		assertEquals(er.getServiceName(), SERVICE_NAME);
+	}
+
+	private EndpointResolver createEndpointResolver() {
+		return createEndpointResolver(SERVICE_NAME, LOCATOR_ENDPOINTS);
+	}
+
+	private EndpointResolver createEndpointResolver(QName serviceName,
+			String locatorEndpoints) {
+		return new EndpointResolver(serviceName, locatorEndpoints) {
+
+			@Override
+			protected ServiceLocator createServiceLocator() {
+				return new ServiceLocator() {
+
+					@Override
+					protected ZooKeeper createZooKeeper(
+							CountDownLatch connectionLatch) throws IOException {
+						connectionLatch.countDown();
+						return zkMock;
+					}
+
+					public List<String> lookup(QName serviceName) {
+						List<String> listOfEndpoints = new ArrayList<String>();
+
+						listOfEndpoints.add(ENDPOINT_1);
+						listOfEndpoints.add(ENDPOINT_2);
+
+						return listOfEndpoints;
+					}
+				};
+			}
+		};
+	}
+}
