7551efba39eb2136b5e65fc5540589a461fe328c
amarkevich <amarkevich@talend.com>
2011-02-09 13:09:53 +0200

    added UsernameToken

--- examples/sts-client/src/main/java/org/apache/esb/sts/client/Invoker.java
+++ examples/sts-client/src/main/java/org/apache/esb/sts/client/Invoker.java
@@ -1,10 +1,15 @@
 package org.apache.esb.sts.client;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import org.apache.cxf.ws.security.tokenstore.SecurityToken;
 import org.apache.cxf.ws.security.trust.STSClient;
+import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
+import org.apache.ws.security.WSConstants;
+import org.apache.ws.security.handler.WSHandlerConstants;
 import org.springframework.beans.factory.InitializingBean;
 
 public class Invoker implements InitializingBean {
@@ -22,13 +27,23 @@ public class Invoker implements InitializingBean {
 			@Override
 			public void run() {
 				try {
+			        Map<String, Object> outProps = new HashMap<String, Object>();
+			        // Manual WSS4J interceptor process
+			        outProps.put(WSHandlerConstants.ACTION, WSHandlerConstants.USERNAME_TOKEN);
+			        outProps.put(WSHandlerConstants.USER, "joe");
+			        outProps.put(WSHandlerConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+			        outProps.put(WSHandlerConstants.PW_CALLBACK_CLASS,
+			                ClientPasswordCallback.class.getName());
+
+			        WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(outProps);
+			        stsClient.getOutInterceptors().add(wssOut);
+			        
 					SecurityToken securityToken = stsClient.requestSecurityToken();
 					System.out.println("securityToken="+securityToken);
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
 			}
 		}, 5000);
 
