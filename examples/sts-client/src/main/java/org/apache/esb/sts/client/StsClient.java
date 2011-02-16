929e0af79baf8a205e540c8042bf7ed0cafd88d6
amarkevich <amarkevich@talend.com>
2011-02-16 13:03:39 +0200

    added parameter for requested token type

--- examples/sts-client/src/main/java/org/apache/esb/sts/client/StsClient.java
+++ examples/sts-client/src/main/java/org/apache/esb/sts/client/StsClient.java
@@ -2,8 +2,6 @@ package org.apache.esb.sts.client;
 
 import java.lang.reflect.UndeclaredThrowableException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -13,13 +11,11 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBusFactory;
 import org.apache.cxf.helpers.DOMUtils;
-import org.apache.cxf.ws.security.SecurityConstants;
 import org.apache.cxf.ws.security.tokenstore.SecurityToken;
 import org.apache.cxf.ws.security.trust.STSClient;
 import org.apache.cxf.ws.security.trust.STSUtils;
-import org.apache.ws.security.components.crypto.Crypto;
-import org.apache.ws.security.components.crypto.CryptoFactory;
 import org.apache.ws.security.message.WSSecUsernameToken;
+import org.opensaml.common.xml.SAMLConstants;
 import org.springframework.beans.factory.InitializingBean;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -28,6 +24,7 @@ public class StsClient implements InitializingBean {
 
 	private STSClient stsClient;
 	private boolean isUsername;
+	private boolean isSaml11;
 
 	public void setStsClient(STSClient stsClient) {
 		this.stsClient = stsClient;
@@ -37,12 +34,19 @@ public class StsClient implements InitializingBean {
 		this.isUsername = isUsername;
 	}
 
+	public void setIsSaml11(boolean isSaml11) {
+		this.isSaml11 = isSaml11;
+	}
+
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		new Timer().schedule(new TimerTask() {
 
 			@Override
 			public void run() {
+				String tokenType = isSaml11
+					? SAMLConstants.SAML1_NS
+					: SAMLConstants.SAML20_NS;
 				try {
 					if (isUsername) {
 						DocumentBuilderFactory dbf = DocumentBuilderFactory
@@ -53,6 +57,8 @@ public class StsClient implements InitializingBean {
 						doc.appendChild(templateElement);
 						Element el = doc.createElementNS(STSUtils.WST_NS_05_12,
 								"TokenType");
+						el.appendChild(doc
+								.createTextNode(tokenType));
 						templateElement.appendChild(el);
 						el = doc.createElementNS(STSUtils.WST_NS_05_12,
 								"OnBehalfOf");
@@ -66,8 +72,13 @@ public class StsClient implements InitializingBean {
 					} else {
 						stsClient.setUseCertificateForConfirmationKeyInfo(true);
 						Document doc = DOMUtils.createDocument();
-						Element templateElement = doc.createElementNS(
-								STSUtils.WST_NS_05_12, "template");
+						Element templateElement = doc.createElement(
+								"template");
+						Element el = doc.createElementNS(STSUtils.WST_NS_05_12,
+								"TokenType");
+						el.appendChild(doc
+								.createTextNode(tokenType));
+						templateElement.appendChild(el);
 						Element keyTypeElement = doc.createElementNS(
 								STSUtils.WST_NS_05_12, "KeyType");
 						keyTypeElement.appendChild(doc
