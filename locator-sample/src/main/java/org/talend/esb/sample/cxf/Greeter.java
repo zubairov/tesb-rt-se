94994e84b60f187071904bf7e414f8531c9e46ac
Zsolt Beothy-Elo <zbeothy-elo@talend.com>
2011-02-22 17:18:19 +0100

    Sample projects obsolete, new ones now under examples/locator

--- locator-sample/src/main/java/org/talend/esb/sample/cxf/Greeter.java
+++
@@ -1,8 +0,0 @@
-package org.talend.esb.sample.cxf;
-
-import javax.jws.WebService;
-
-@WebService(targetNamespace = "http://talend.org/esb/examples/", name = "Greeter")
-public interface Greeter {
-	String greetMe(String requestType);
-}
