c0d725cc494bbe65051ee4ed11cc6609358e11ec
Zsolt Beothy-Elo <zbeothy-elo@talend.com>
2011-02-24 10:42:53 +0100

    Remember last good address in locator based selection strategy

--- locator/src/test/java/org/talend/esb/locator/LocatorSelectionStrategyTest.java
+++ locator/src/test/java/org/talend/esb/locator/LocatorSelectionStrategyTest.java
@@ -1,10 +1,17 @@
 package org.talend.esb.locator;
 
-import static org.easymock.EasyMock.*;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.collection.IsIn.isIn;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -16,10 +23,13 @@ import org.apache.cxf.message.Exchange;
 import org.apache.cxf.service.Service;
 import org.hamcrest.Matcher;
 import org.junit.Test;
+import org.junit.Before;
 
 public class LocatorSelectionStrategyTest {
-	
-	public static final QName SERVICE_NAME = new QName("nsp", "name");
+
+	public static final QName SERVICE_NAME_1 = new QName("nsp1", "name1");
+
+	public static final QName SERVICE_NAME_2 = new QName("nsp2", "name2");
 
 	public static final String ENDPOINT_1 = "endpoint1";
 
@@ -27,24 +37,44 @@ public class LocatorSelectionStrategyTest {
 
 	public static final String ENDPOINT_3 = "endpoint3";
 	
-	public static final List<String> ENDPOINTS = 
+	public static final String ENDPOINT_4 = "endpoint4";
+
+	public static final String ENDPOINT_5 = "endpoint5";
+
+	public static final List<String> ENDPOINTS_1 = 
 		Arrays.asList(ENDPOINT_1, ENDPOINT_2, ENDPOINT_3);
 	
+	public static final List<String> ENDPOINTS_2 = 
+		Arrays.asList(ENDPOINT_4, ENDPOINT_5);
+
+	public static final List<String> ENDPOINTS_1A = 
+		Arrays.asList(ENDPOINT_1, ENDPOINT_2, ENDPOINT_3);
+	
+	public static final List<String> ENDPOINTS_1B = 
+		Arrays.asList(ENDPOINT_4, ENDPOINT_5);
+
 	public static final Matcher<Collection<String>> EMPTY_ENDPOINT_COLLECTION = empty();
 
+	private static final Exchange EXCHANGE_1 = createExchangeMock(SERVICE_NAME_1);
+
+	private static final Exchange EXCHANGE_2 = createExchangeMock(SERVICE_NAME_2);
+
+	private ServiceLocator sl = createMock(ServiceLocator.class);
+
+	private LocatorSelectionStrategy strategy;
+	
+	@Before
+	public void setup() {
+		strategy = new LocatorSelectionStrategy();
+		strategy.setServiceLocator(sl);
+	}
+	
 	@Test
 	public void getAlternateAddressesReturnsListFromLocator() throws Exception {
-		
-		ServiceLocator sl = createMock(ServiceLocator.class);
-		Exchange exchange = createExchangeMock(SERVICE_NAME);
-
-		expect(sl.lookup(SERVICE_NAME)).andReturn(ENDPOINTS);
+		expect(sl.lookup(SERVICE_NAME_1)).andReturn(ENDPOINTS_1);
 		replay(sl);
 		
-		LocatorSelectionStrategy strategy = new LocatorSelectionStrategy();
-		strategy.setServiceLocator(sl);
-		
-		List<String> alternates = strategy.getAlternateAddresses(exchange);
+		List<String> alternates = strategy.getAlternateAddresses(EXCHANGE_1);
 
 		assertThat(alternates, containsInAnyOrder(ENDPOINT_1, ENDPOINT_2, ENDPOINT_3));
 		verify(sl);
@@ -52,20 +82,73 @@ public class LocatorSelectionStrategyTest {
 	
 	@Test
 	public void getAlternateAddressesLocatorThrowsException() throws Exception {
-		
-		ServiceLocator sl = createMock(ServiceLocator.class);
-		Exchange exchange = createExchangeMock(SERVICE_NAME);
-
-		expect(sl.lookup(SERVICE_NAME)).andThrow(new ServiceLocatorException());
+		expect(sl.lookup(SERVICE_NAME_1)).andThrow(new ServiceLocatorException());
 		replay(sl);
-		
-		LocatorSelectionStrategy strategy = new LocatorSelectionStrategy();
-		strategy.setServiceLocator(sl);
-		
-		List<String> alternates = strategy.getAlternateAddresses(exchange);
+				
+		List<String> alternates = strategy.getAlternateAddresses(EXCHANGE_1);
 
 		assertThat(alternates, EMPTY_ENDPOINT_COLLECTION);
 		verify(sl);
+	}
+
+	@Test
+	public void getPrimaryAddressReturnsElementFromLocator() throws Exception {
+		expect(sl.lookup(SERVICE_NAME_1)).andReturn(ENDPOINTS_1);
+		replay(sl);
+		
+		String address = strategy.getPrimaryAddress(EXCHANGE_1);
+
+		assertThat(address, isIn(ENDPOINTS_1));
+		verify(sl);
+	}
+
+	@Test
+	public void secondGetPrimaryAddressCallWithSameExchangeReturnsSameAsFirstCall() throws Exception {
+		expect(sl.lookup(SERVICE_NAME_1)).andReturn(ENDPOINTS_1);
+		replay(sl);
+
+		String firstAddress = strategy.getPrimaryAddress(EXCHANGE_1);
+		String secondAddress = strategy.getPrimaryAddress(EXCHANGE_1);
+
+		assertThat(secondAddress, equalTo(firstAddress));
+		verify(sl);
+	}
+
+	@Test
+	public void secondGetPrimaryAddressCallWithDifferentExchangeReturnsDifferentAsFirstCall() throws Exception {
+		expect(sl.lookup(SERVICE_NAME_1)).andReturn(ENDPOINTS_1);
+		expect(sl.lookup(SERVICE_NAME_2)).andReturn(ENDPOINTS_2);
+		replay(sl);
+		
+		strategy.getPrimaryAddress(EXCHANGE_1);
+		String secondAddress = strategy.getPrimaryAddress(EXCHANGE_2);
+
+		assertThat(secondAddress, isIn(ENDPOINTS_2));
+		verify(sl);
+	}
+
+	@Test
+	public void secondGetPrimaryAddressReturnsNewAddressWhenGetAlternateAddressCalledInBetween()
+	throws Exception{
+		expect(sl.lookup(SERVICE_NAME_1)).andReturn(ENDPOINTS_1A);
+		expect(sl.lookup(SERVICE_NAME_1)).andReturn(ENDPOINTS_1A);
+		expect(sl.lookup(SERVICE_NAME_1)).andReturn(ENDPOINTS_1B);
+		replay(sl);
+	
+		String firstAddress = strategy.getPrimaryAddress(EXCHANGE_1);
+		strategy.getAlternateAddresses(EXCHANGE_1);
+		String secondAddress = strategy.getPrimaryAddress(EXCHANGE_1);
+
+		assertThat(secondAddress, not(equalTo(firstAddress)));
+		assertThat(secondAddress, isIn(ENDPOINTS_1B));
+		verify(sl);
+	}
+
+	@Test
+	public void selectAlternateAddressReturnsOneOfTheAddressesPassedAsParamter() {
+		List<String> endpoints = new ArrayList<String>(ENDPOINTS_1); 
+		String address = strategy.selectAlternateAddress(endpoints);
+		assertThat(address, isIn(ENDPOINTS_1));
 	}
 
 	private static Exchange createExchangeMock(QName serviceName) {
@@ -73,7 +156,7 @@ public class LocatorSelectionStrategyTest {
 		Endpoint ep = createMock(Endpoint.class);
 		Exchange exchange = createMock(Exchange.class);
 
-		expect(service.getName()).andStubReturn(SERVICE_NAME);
+		expect(service.getName()).andStubReturn(serviceName);
 		expect(ep.getService()).andStubReturn(service);
 		expect(exchange.getEndpoint()).andStubReturn(ep);
 		
