99af8ddd3d2dacbee45b58c458c773ceac03d7da
Christian Schneider <cschneider@talend.com>
2011-03-01 13:58:06 +0100

    fix build

--- sam/Dummy/DummyService/src/test/java/ch/zurich/caseservice/impl/CaseServiceTest.java
+++ sam/Dummy/DummyService/src/test/java/ch/zurich/caseservice/impl/CaseServiceTest.java
@@ -12,7 +12,7 @@ public class CaseServiceTest {
 	public void addCaseTest() {
 		CaseServiceImpl caseServiceImpl = new CaseServiceImpl();
 		try {
-			caseServiceImpl.addCase("1", "a", "10,99€", "contractor", null,
+			caseServiceImpl.addCase("1", "a", "10,99", "contractor", null,
 					new Holder<String>(), new Holder<Boolean>());
 		} catch (AddCaseFault_Exception e) {
 			e.printStackTrace();
