3dd446901ffaa6a6cb2273a2ec849d03c3cc2b93
jbonofre <jbonofre@apache.org>
2011-03-10 17:42:05 +0100

    [TESB-879] Add Talend Job API [TESB-880] Add Talend Job launcher.

---
+++ job/launcher/src/main/java/org/talend/esb/job/launcher/Main.java
@@ -0,0 +1,15 @@
+package org.talend.esb.job.launcher;
+
+/**
+ * Main Talend job launcher.
+ */
+public class Main {
+
+    /**
+     * Method callback at launcher init.
+     */
+    public void init() {
+        // empty
+    }
+
+}
