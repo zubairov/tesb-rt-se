f959e2756eefc26f477b524ec6604f89f23dd07f
Andreas Mattes <andreasmattes@operamail.com>
2011-04-21 02:03:49 +0200

    change all add-ons subdirs to lowercase

--- talend-esb/bin/locatorCli.cmd
+++ talend-esb/bin/locatorCli.cmd
@@ -24,7 +24,7 @@ setlocal
 set TESB_ADDONS=%~dp0..\add-ons
 REM set ZOO_LOG4J_PROP=INFO,CONSOLE
 
-set CLASSPATH="%TESB_ADDONS%/Locator/locator-4.0.jar;%TESB_ADDONS%/lib/zookeeper-3.3.3.jar;%TESB_ADDONS%/lib/log4j-1.2.15.jar"
+set CLASSPATH="%TESB_ADDONS%/locator/locator-4.0.jar;%TESB_ADDONS%/lib/zookeeper-3.3.3.jar;%TESB_ADDONS%/lib/log4j-1.2.15.jar"
 
 echo on
 java -cp "%CLASSPATH%" org.talend.esb.locator.ServiceLocatorMain  %*
