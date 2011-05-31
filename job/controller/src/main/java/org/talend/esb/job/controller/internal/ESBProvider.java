94c38361e73bdd8462e126035460673c4be1213d
amarkevich <amarkevich@talend.com>
2011-05-31 19:29:11 +0300

    job controller: added implementation for generic provider

--- job/controller/src/main/java/org/talend/esb/job/controller/internal/ESBProvider.java
+++ job/controller/src/main/java/org/talend/esb/job/controller/internal/ESBProvider.java
@@ -22,8 +22,11 @@ package org.talend.esb.job.controller.internal;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.annotation.Resource;
 import javax.xml.namespace.QName;
 import javax.xml.transform.Source;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
 
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
@@ -41,6 +44,9 @@ class ESBProvider implements javax.xml.ws.Provider<javax.xml.transform.Source> {
 	private String publishedEndpointUrl;
 	private QName serviceName;
 	private QName portName;
+
+	@Resource
+	private WebServiceContext context;
 
 	public ESBProvider(String publishedEndpointUrl,
 			final QName serviceName,
@@ -90,26 +96,40 @@ class ESBProvider implements javax.xml.ws.Provider<javax.xml.transform.Source> {
         }
 	}
 	
+	// TODO: add dynamic WebMethod
 	@Override
-//	@javax.jws.WebMethod(operationName = "getCustomersByName", action = "http://talend.org/esb/service/job/invoke")
+	@javax.jws.WebMethod(operationName = "getCustomersByName", action = "http://talend.org/esb/service/job/invoke")
 //	@javax.jws.WebResult(name = "jobOutput", targetNamespace = "http://talend.org/esb/service/job",
 //	partName = "response")
 	public Source invoke(Source request) {
-		//callbacks.get(key)
+		QName operationName = (QName)context.getMessageContext().get(MessageContext.WSDL_OPERATION);
+		RuntimeESBProviderCallback esbProviderCallback =
+			getESBProviderCallback(operationName.getLocalPart());
+		if(esbProviderCallback == null) {
+			throw new RuntimeException("Handler for operation '" + operationName.getLocalPart() + "' cannot be found");
+		}
 		try {
 			org.dom4j.io.DocumentResult docResult = new org.dom4j.io.DocumentResult();
-			factory.newTransformer().transform(request,
-					docResult);
-			org.dom4j.Document requestDoc = docResult
-					.getDocument();
-			 System.out.println("request: " +
-			 requestDoc.asXML());
+			factory.newTransformer().transform(request, docResult);
+			org.dom4j.Document requestDoc = docResult.getDocument();
+			
+			System.out.println("request: " +requestDoc.asXML());
+			Object result = esbProviderCallback.invoke(requestDoc);
+			
+			if(result instanceof org.dom4j.Document) {
+				return new org.dom4j.io.DocumentSource(
+						(org.dom4j.Document)result);
+			} else {
+				throw new RuntimeException("Provider return incompatible object: " + result.getClass().getName());
+			}
+
+		} catch (RuntimeException e) {
+			throw e;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
-		
-		return null;
 	}
 
 	public RuntimeESBProviderCallback createESBProviderCallback(String operationName) {
