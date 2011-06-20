2b6b780239d784c857c3efdb15a355213fc507bc
amarkevich <amarkevich@talend.com>
2011-06-20 13:00:26 +0300

    untabify

--- job/controller/src/main/java/org/talend/esb/job/controller/internal/ESBProviderKey.java
+++ job/controller/src/main/java/org/talend/esb/job/controller/internal/ESBProviderKey.java
@@ -23,27 +23,27 @@ import javax.xml.namespace.QName;
 
 public class ESBProviderKey {
 
-	private final QName serviceName;
-	private final QName portName;
+    private final QName serviceName;
+    private final QName portName;
 
-	public ESBProviderKey(
-			final QName serviceName,
-			final QName portName) {
-		this.serviceName = serviceName;
-		this.portName = portName;
-	}
+    public ESBProviderKey(
+            final QName serviceName,
+            final QName portName) {
+        this.serviceName = serviceName;
+        this.portName = portName;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if(obj instanceof ESBProviderKey) {
-			ESBProviderKey anotherObj = (ESBProviderKey)obj;
-			return serviceName.equals(anotherObj.serviceName) && portName.equals(anotherObj.portName);
-		}
-		return super.equals(obj);
-	}
-	
-	@Override
-	public int hashCode() {
-		return serviceName.hashCode() ^ portName.hashCode();
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof ESBProviderKey) {
+            ESBProviderKey anotherObj = (ESBProviderKey)obj;
+            return serviceName.equals(anotherObj.serviceName) && portName.equals(anotherObj.portName);
+        }
+        return super.equals(obj);
+    }
+    
+    @Override
+    public int hashCode() {
+        return serviceName.hashCode() ^ portName.hashCode();
+    }
 }
