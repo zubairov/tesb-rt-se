256de7eddc0d62b91f746cbc5791237f385f6539
Serhii Nesteruk <serhii.nesteruk@gmail.com>
2011-07-15 12:12:03 +0300

    TESB-2091 updating starting derby in SAM server

---
+++ sam/sam-server-war/src/main/java/org/talend/esb/sam/server/listener/DerbyStarterContextListener.java
@@ -0,0 +1,44 @@
+package org.talend.esb.sam.server.listener;
+
+import java.net.InetAddress;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.derby.drda.NetworkServerControl;
+
+public class DerbyStarterContextListener implements ServletContextListener {
+	private NetworkServerControl server;
+	private boolean startDerby;
+
+	public DerbyStarterContextListener() {
+		startDerby = false;
+		String startDerbyProperty = System.getenv("org.talend.esb.sam.server.embedded");
+		if ((startDerbyProperty != null)
+				&& (startDerbyProperty.toUpperCase().equals("TRUE"))) {
+			startDerby = true;
+		}
+	}
+
+	public void contextDestroyed(ServletContextEvent arg0) {
+		if (startDerby) {
+			try {
+				server.shutdown();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void contextInitialized(ServletContextEvent arg0) {
+		if (startDerby) {
+			try {
+				server = new NetworkServerControl(
+						InetAddress.getByName("localhost"), 1527);
+				server.start(null);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
