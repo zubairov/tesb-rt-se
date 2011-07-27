e3790081e9223ea9eb52f52ba761276420c98293
rminko <rnminko@gmail.com>
2011-07-28 01:27:41 +0300

    Initial implementation of LocatorProxyServiceImpl

--- locator-proxy/locator-proxy-service/src/main/java/org/talend/esb/locator/proxy/service/LocatorProxyServiceImpl.java
+++ locator-proxy/locator-proxy-service/src/main/java/org/talend/esb/locator/proxy/service/LocatorProxyServiceImpl.java
@@ -19,70 +19,129 @@
  */
 package org.talend.esb.locator.proxy.service;
 
+import java.util.List;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.wsaddressing.W3CEndpointReference;
+import javax.xml.ws.wsaddressing.W3CEndpointReferenceBuilder;
 
-import org.talend.esb.locator.proxy.service.types.EndpointReferenceListType;
 import org.talend.esb.servicelocator.client.ServiceLocator;
 import org.talend.esb.servicelocator.client.ServiceLocatorException;
-import org.talend.esb.servicelocator.cxf.internal.LocatorRegistrar;
+import org.talend.esb.locator.proxy.service.types.EndpointReferenceListType;
 
 public class LocatorProxyServiceImpl implements LocatorProxyService {
 
-    private static final Logger LOG = Logger.getLogger(LocatorProxyServiceImpl.class.getPackage().getName());
-	
+	private static final Logger LOG = Logger
+			.getLogger(LocatorProxyServiceImpl.class.getPackage().getName());
+
 	private ServiceLocator locatorClient;
 
-    public void setServiceLocator(ServiceLocator locatorClient) {
-        this.locatorClient = locatorClient;
-        if (LOG.isLoggable(Level.FINE)) {
-            LOG.log(Level.FINE, "Locator client was set.");
-        }
-    }
-	
+	private Random random = new Random();
+
+	public void setServiceLocator(ServiceLocator locatorClient) {
+		this.locatorClient = locatorClient;
+		if (LOG.isLoggable(Level.FINE)) {
+			LOG.log(Level.FINE, "Locator client was set for proxy service.");
+		}
+	}
+
 	@Override
-	public void registerEndpoint(QName serviceName, String endpointURL) {
+	public void registerEndpoint(QName serviceName, String endpointURL)
+			throws InterruptedExceptionFault, ServiceLocatorFault {
 		try {
 			locatorClient.register(serviceName, endpointURL);
 		} catch (ServiceLocatorException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new InterruptedExceptionFault("", e);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new ServiceLocatorFault("", e);
 		}
-		
-	}
-	
-	@Override
-	public W3CEndpointReference lookupEndpoint(QName serviceName) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
-	public EndpointReferenceListType lookupEndpoints(QName serviceName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean unregisterEnpoint(QName serviceName, String endpointURL) {
+	public boolean unregisterEnpoint(QName serviceName, String endpointURL)
+			throws InterruptedExceptionFault, ServiceLocatorFault {
 		try {
 			locatorClient.unregister(serviceName, endpointURL);
-			return true;
 		} catch (ServiceLocatorException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new InterruptedExceptionFault("", e);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new ServiceLocatorFault("", e);
 		}
-		
-		return false;
+		return true;
 	}
 
+	@Override
+	public W3CEndpointReference lookupEndpoint(QName serviceName)
+			throws InterruptedExceptionFault, ServiceLocatorFault {
+		List<String> names = null;
+		String adress;
+		try {
+			names = locatorClient.getEndpointNames(serviceName);
+		} catch (ServiceLocatorException e) {
+			throw new InterruptedExceptionFault("", e);
+		} catch (InterruptedException e) {
+			throw new ServiceLocatorFault("", e);
+		}
+		if (names != null && !names.isEmpty()) {
+			names = getRotatedList(names);
+			adress = names.get(0);
+		} else {
+			if (LOG.isLoggable(Level.WARNING)) {
+				LOG.log(Level.WARNING, "lookup Endpoint " + serviceName
+						+ " failed, service is not known.");
+			}
+			return null;
+		}
+		return buildEndpoint(serviceName, adress);
+	}
+
+	@Override
+	public EndpointReferenceListType lookupEndpoints(QName serviceName) throws InterruptedExceptionFault, ServiceLocatorFault {
+		List<String> names = null;
+		List<W3CEndpointReference> endpointRefList = Collections.emptyList();
+		String adress;
+		try {
+			names = locatorClient.getEndpointNames(serviceName);
+		} catch (ServiceLocatorException e) {
+			throw new InterruptedExceptionFault("", e);
+		} catch (InterruptedException e) {
+			throw new ServiceLocatorFault("", e);
+		}
+		if (names != null && !names.isEmpty()) {
+			for (int i = 0; i < names.size(); i++) {
+				adress = names.get(i);
+				endpointRefList.add(buildEndpoint(serviceName, adress));
+			}
+			adress = names.get(0);
+		} else {
+			if (LOG.isLoggable(Level.WARNING)) {
+				LOG.log(Level.WARNING, "lookup Endpoint " + serviceName
+						+ " failed, service is not known.");
+			}
+			return null;
+		}
+		return (EndpointReferenceListType)endpointRefList;
+	}
+
+	private List<String> getRotatedList(List<String> strings) {
+		int index = random.nextInt(strings.size());
+		List<String> rotated = new ArrayList<String>();
+		for (int i = 0; i < strings.size(); i++) {
+			rotated.add(strings.get(index));
+			index = (index + 1) % strings.size();
+		}
+		return rotated;
+	}
+
+	private W3CEndpointReference buildEndpoint(QName serviceName, String adress) {
+		W3CEndpointReferenceBuilder builder = new W3CEndpointReferenceBuilder();
+		builder.serviceName(serviceName);
+		builder.address(adress);
+		return builder.build();
+	}
 }
