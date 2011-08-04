c2dd10e2e76c47fb5abae8f926cc52e463375130
Pavel Vasilchenko <pavel.s.vasilchenko@gmail.com>
2011-08-04 16:52:08 +0300

    - create simple client which send request to locator proxy.

--- examples/locator-proxy/client/src/main/java/demo/client/Client.java
+++ examples/locator-proxy/client/src/main/java/demo/client/Client.java
@@ -18,43 +18,38 @@
  * #L%
  */
 /*******************************************************************************
-*
-* Copyright (c) 2011 Talend Inc. - www.talend.com
-* All rights reserved.
-*
-* This program and the accompanying materials are made available
-* under the terms of the Apache License v2.0
-* which accompanies this distribution, and is available at
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-*******************************************************************************/
+ *
+ * Copyright (c) 2011 Talend Inc. - www.talend.com
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Apache License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *******************************************************************************/
 
 package demo.client;
 
+import javax.xml.namespace.QName;
+import javax.xml.ws.wsaddressing.W3CEndpointReference;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.talend.esb.locator.proxy.service.LocatorProxyService;
 import demo.common.Greeter;
 
 public class Client {
 
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/client.xml");
-		Greeter client = (Greeter) context.getBean("greeterService");
+		LocatorProxyService client = (LocatorProxyService) context.getBean("locatorProxyService");
 
-		String response = null;
-		for (int i = 0; i < 10; i++) {
-			System.out.println("BEGIN...");
-
-			response = client.greetMe("MyName#" + i);
-			System.out.println("Response from the service: ");
-			System.out.println(response);
-			
-			System.out.println("END...");
-
-		}
-
+		W3CEndpointReference endpointReference = client.lookupEndpoint(new QName("http://services.talend.org/CRMService", "CRMServiceProvider"));
+		System.out.println(endpointReference.toString());
+		
 		Thread.sleep(2000);
 		context.close();
-		System.exit(0); 
+		System.exit(0);
 
 	}
 }
