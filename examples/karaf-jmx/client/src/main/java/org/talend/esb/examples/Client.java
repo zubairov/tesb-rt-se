a43ea18c207710b0fb9c3ab5e1bc497a9f8c5f72
Alexander Osadchiy <alexander.g.osadchiy@gmail.com>
2011-09-01 09:33:11 +0300

    karaf-jmx example: - add spring support; - implement start method; - implement stop method; - add notification listener; - minor improvements in build.

--- examples/karaf-jmx/client/src/main/java/org/talend/esb/examples/Client.java
+++ examples/karaf-jmx/client/src/main/java/org/talend/esb/examples/Client.java
@@ -4,41 +4,100 @@ import java.util.HashMap;
 
 import javax.management.MBeanServerConnection;
 import javax.management.remote.JMXConnector;
+
 import org.apache.karaf.features.management.FeaturesServiceMBean;
 
-import org.talend.esb.examples.impl.ApplicationManagerImpl;
+import org.osgi.jmx.framework.FrameworkMBean;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
+
+	private static ApplicationManager applicationManager;
+	private HashMap<String, String[]> environment;
+	private String serviceURL;
+	private String repositoryURL;
+	private String featureName;
+
+	public void setApplicationManager(ApplicationManager applicationManager) {
+		Client.applicationManager = applicationManager;
+	}
+	
+	public HashMap<String, String[]> getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(HashMap<String, String[]> environment) {
+		this.environment = environment;
+	}
+
+	public String getServiceURL() {
+		return serviceURL;
+	}
+	
+	public void setServiceURL(String serviceURL) {
+		this.serviceURL = serviceURL;
+	}
+	
+	public String getRepositoryURL() {
+		return repositoryURL;
+	}
+
+	public void setRepositoryURL(String repositoryURL) {
+		this.repositoryURL = repositoryURL;
+	}
+
+	public String getFeatureName() {
+		return featureName;
+	}
+
+	public void setFeatureName(String featureName) {
+		this.featureName = featureName;
+	}
 
 	public static void main(String args[]) {
 
 		try {
 
-			ApplicationManagerImpl applicationManager = new ApplicationManagerImpl();
-			
-			HashMap<String, String[]> env = applicationManager.authorize();
-			
+			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+					new String[] { "META-INF/spring/client-beans.xml" });
+
+			Client client = (Client) context.getBean("Client");
+
 			JMXConnector jmxc = applicationManager.createRMIconnector(
-					"service:jmx:rmi://localhost:44444/jndi/rmi://localhost:1099/karaf-tesb",
-					env);
+					client.getServiceURL(), client.getEnvironment());
 
-			MBeanServerConnection mbsc = applicationManager.getMBeanServerConnection(jmxc);
+			MBeanServerConnection mbsc = applicationManager
+					.getMBeanServerConnection(jmxc);
 
-			FeaturesServiceMBean mbeanProxy = applicationManager.createProxy(mbsc);
+			FeaturesServiceMBean featuresServiceMBeanProxy = applicationManager
+					.createFeaturesServiceMBeanProxy(mbsc);
 
-			applicationManager.addRepository(mbeanProxy);
+			FrameworkMBean osgiFrameworkProxy = applicationManager
+					.createOsgiFrameworkMBeanProxy(mbsc);
 
-			applicationManager.installFeature(mbeanProxy);
+			applicationManager.addRepository(featuresServiceMBeanProxy,
+					client.getRepositoryURL());
 
-			applicationManager.uninstallFeature(mbeanProxy);
-			
-			applicationManager.removeRepository(mbeanProxy);
+			applicationManager.installFeature(featuresServiceMBeanProxy,
+					client.getFeatureName());
+
+			applicationManager.startBundle(osgiFrameworkProxy, 131);
+
+			applicationManager.stopBundle(osgiFrameworkProxy, 131);
+
+			applicationManager.uninstallFeature(featuresServiceMBeanProxy,
+					client.getFeatureName());
+
+			applicationManager.removeRepository(featuresServiceMBeanProxy,
+					client.getRepositoryURL());
 
 			applicationManager.closeConnection(jmxc);
+			
+			Thread.sleep(5000);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
--- examples/karaf-jmx/common/src/main/java/org/talend/esb/examples/impl/ApplicationManagerImpl.java
+++ examples/karaf-jmx/common/src/main/java/org/talend/esb/examples/impl/ApplicationManagerImpl.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.HashMap;
 
+import javax.management.InstanceNotFoundException;
 import javax.management.JMX;
 import javax.management.MBeanServerConnection;
 import javax.management.MalformedObjectNameException;
@@ -13,74 +14,93 @@ import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 
 import org.apache.karaf.features.management.FeaturesServiceMBean;
+import org.osgi.jmx.framework.FrameworkMBean;
 import org.talend.esb.examples.ApplicationManager;
+import org.talend.esb.examples.ClientListener;
 
 public class ApplicationManagerImpl implements ApplicationManager {
 
+	private ClientListener clientListener;
+	
 	public JMXConnector createRMIconnector(String serviceUrl,
 			HashMap<String, String[]> environment)
 			throws MalformedURLException, IOException {
-		echo("\nCreate an RMI connector client and "
-				+ "connect it to the RMI connector server");
 		JMXServiceURL url = new JMXServiceURL(serviceUrl);
 		JMXConnector jmxc = JMXConnectorFactory.connect(url, environment);
 		return jmxc;
 	}
 
+	public void closeConnection(JMXConnector jmxc) throws IOException {
+		jmxc.close();
+	}
+
 	public MBeanServerConnection getMBeanServerConnection(JMXConnector jmxc)
 			throws IOException {
-		echo("\nGet an MBeanServerConnection");
 		MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
 		return mbsc;
 	}
 
-	public FeaturesServiceMBean createProxy(MBeanServerConnection mbsc)
-			throws MalformedObjectNameException, NullPointerException {
-		echo("\n>>> Perform operations on Tesb MBean <<<");
+	public FeaturesServiceMBean createFeaturesServiceMBeanProxy(
+			MBeanServerConnection mbsc) throws MalformedObjectNameException,
+			NullPointerException, InstanceNotFoundException, IOException {
 		ObjectName mbeanName = new ObjectName(
 				"org.apache.karaf:type=features,name=tesb");
-		FeaturesServiceMBean mbeanProxy = JMX.newMBeanProxy(mbsc, mbeanName,
-				FeaturesServiceMBean.class, false);
-		return mbeanProxy;
+		FeaturesServiceMBean featuresServiceMBeanProxy = JMX.newMBeanProxy(
+				mbsc, mbeanName, FeaturesServiceMBean.class, true);
+
+		mbsc.addNotificationListener(mbeanName, clientListener, null, null);
+
+		return featuresServiceMBeanProxy;
 	}
 
-	public HashMap<String, String[]> authorize() {
-		echo("\nInitialize the environment map");
-		HashMap<String, String[]> env = new HashMap<String, String[]>();
-		String[] credentials = new String[] { "karaf", "karaf" };
-		env.put("jmx.remote.credentials", credentials);
-		return env;
+	public FrameworkMBean createOsgiFrameworkMBeanProxy(
+			MBeanServerConnection mbsc) throws MalformedObjectNameException,
+			NullPointerException {
+		ObjectName mbeanName = new ObjectName(
+				"osgi.core:type=framework,version=1.5");
+		FrameworkMBean osgiFrameworkProxy = JMX.newMBeanProxy(mbsc, mbeanName,
+				FrameworkMBean.class, false);	
+		return osgiFrameworkProxy;
 	}
 
-	public void addRepository(FeaturesServiceMBean mbeanProxy) throws Exception {
-		mbeanProxy
-				.addRepository("mvn:com.talend.if.examples/osgi/1.0/xml/features");
+	public void addRepository(FeaturesServiceMBean featuresServiceMBeanProxy,
+			String url) throws Exception {
+		featuresServiceMBeanProxy.addRepository(url);
 	}
 
-	public void closeConnection(JMXConnector jmxc) throws IOException {
-		echo("\nClose the connection to the server");
-		jmxc.close();
-		echo("\nBye! Bye!");
-	}
-
-	public void uninstallFeature(FeaturesServiceMBean mbeanProxy)
+	public void removeRepository(
+			FeaturesServiceMBean featuresServiceMBeanProxy, String url)
 			throws Exception {
-		mbeanProxy.uninstallFeature("tif-example-claimcheck");
+		featuresServiceMBeanProxy.removeRepository(url);
 	}
 
-	public void removeRepository(FeaturesServiceMBean mbeanProxy)
+	public void installFeature(FeaturesServiceMBean featuresServiceMBeanProxy,
+			String featureName) throws Exception {
+		featuresServiceMBeanProxy.installFeature(featureName);
+	}
+
+	public void uninstallFeature(
+			FeaturesServiceMBean featuresServiceMBeanProxy, String featureName)
 			throws Exception {
-		mbeanProxy
-				.removeRepository("mvn:com.talend.if.examples/osgi/1.0/xml/features");
+		featuresServiceMBeanProxy.uninstallFeature(featureName);
 	}
 
-	public void installFeature(FeaturesServiceMBean mbeanProxy)
+	public void startBundle(FrameworkMBean osgiFrameworkProxy, long bundleNumber)
 			throws Exception {
-		mbeanProxy.installFeature("tif-example-claimcheck");
+		osgiFrameworkProxy.startBundle(bundleNumber);
 	}
 
-	private static void echo(String msg) {
-		System.out.println(msg);
+	public void stopBundle(FrameworkMBean osgiFrameworkProxy, long bundleNumber)
+			throws Exception {
+		osgiFrameworkProxy.stopBundle(bundleNumber);
+	}
+
+	public ClientListener getClientListener() {
+		return clientListener;
+	}
+
+	public void setClientListener(ClientListener clientListener) {
+		this.clientListener = clientListener;
 	}
 
 }
