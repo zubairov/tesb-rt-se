08c1d300e506229953b2beaaaed3c94a8564427f
PavelVasilchenko <pavel.s.vasilchenko@gmail.com>
2011-09-19 17:19:49 +0300

    - update client

--- examples/locator-rest-proxy/client/src/main/java/org/talend/esb/locator/rest/proxy/example/RESTClient.java
+++ examples/locator-rest-proxy/client/src/main/java/org/talend/esb/locator/rest/proxy/example/RESTClient.java
@@ -5,9 +5,17 @@ package org.talend.esb.locator.rest.proxy.example;
  * This class is not complete
  */
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Collections;
 import java.util.List;
 
 import javax.ws.rs.core.Response;
@@ -21,48 +29,68 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.RequestEntity;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.apache.cxf.jaxrs.client.ResponseReader;
+import org.apache.cxf.jaxrs.client.ServerWebApplicationException;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.talend.esb.locator.rest.proxy.service.LocatorProxyService;
+import org.talend.esb.locator.rest.proxy.service.types.EndpointReferenceListType;
 import org.talend.esb.locator.rest.proxy.service.types.RegisterEndpointRequestType;
 
 public final class RESTClient {
-	private static final String baseAddress = "http://localhost:8040/services/ServiceLocatorRestProxyService/locator/endpoint";
-
-	private RESTClient() {
+	
+	private RESTClient() throws IOException {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "META-INF/spring/beans.xml" });
+		LocatorProxyService client = (LocatorProxyService) context.getBean("restClient");
+		try {
+			registerExample(client);
+			lookupEndpointsExample(client);
+			unregisterExample(client);
+			lookupEndpointsExample(client);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String args[]) throws java.lang.Exception {
-		WebClient wc = WebClient.create(baseAddress);
-		RegisterEndpointRequestType rert = new RegisterEndpointRequestType();
-		rert.setEndpointURL("http://MyEndpoint.org");
-		rert.setServiceName("{http://MyService.org}MyService");
-		Response resp = wc.post(rert);
-		int status = resp.getStatus();
-		System.out
-				.println("Using a simple JAX-RS proxy to get all the persons...");
-		// String baseAddress =
-		// "http://localhost:8040/services/ServiceLocatorRestProxyService/locator/";
-		/*LocatorProxyService lps = JAXRSClientFactory.create(baseAddress,
-				LocatorProxyService.class);
-		RegisterEndpointRequestType rert = new RegisterEndpointRequestType();
-		rert.setEndpointURL("http://MyEndpoint.org");
-		rert.setServiceName("{http://MyService.org}MyService");
-		System.out.println("Invoking registerEndpoint method: serviceName="
-				+ rert.getServiceName() + "  enpointURL="
-				+ rert.getEndpointURL());
-		Response response = lps.registerEndpoint(rert);
-		System.out.println("Response status code: " + response.getStatus());
-		if (response.getStatus() == 200) {
-			System.out.println("Location ="
-					+ response.getMetadata().getFirst("Location").toString());
-		}*/
-//		Response response;
-//		System.out
-//				.println("Invoking registerEndpoint method: registerEndpoint1.xml");
-//		RESTClient client = new RESTClient();
-//		String input =client.getClass().getResource("registerEndpoint1.xml").getFile();
-//		response = wc.post(client);
-//		int k=0;
+		new RESTClient();
+	}
+	
+	private void registerExample(LocatorProxyService client) {
+		System.out.println("Register service with endpoints");
+		RegisterEndpointRequestType registerEndpointRequestType = new RegisterEndpointRequestType();
+		registerEndpointRequestType.setEndpointURL("http://services.talend.org/TestEndpoint");
+		registerEndpointRequestType.setServiceName("{http://service.proxy.locator.esb.talend.org}LocatorProxyServiceImpl");
+		try {
+			client.registerEndpoint(registerEndpointRequestType);	
+		} catch (ServerWebApplicationException ex) {
+			System.err.println(ex.getMessage());
+		}
+		
+	}
+	
+	private void unregisterExample(LocatorProxyService client) throws UnsupportedEncodingException {
+		System.out.println("Unregister endpoints");
+		try {
+			client.unregisterEndpoint(URLEncoder.encode("{http://service.proxy.locator.esb.talend.org}LocatorProxyServiceImpl", "UTF-8"), URLEncoder.encode("http://services.talend.org/TestEndpoint", "UTF-8"));	
+		} catch (ServerWebApplicationException ex) {
+			System.err.println(ex.getResponse().getStatus() + ": " + ex.getMessage());
+		}
+		
+	}
+	
+	private void lookupEndpointsExample(LocatorProxyService client) throws IOException {
+		System.out.println("lookupEndpoints");
+		try {
+			EndpointReferenceListType r = client.lookupEndpoints(URLEncoder.encode("{http://service.proxy.locator.esb.talend.org}LocatorProxyServiceImpl", "UTF-8"), null);
+			if(r.getReturn().size() > 0)
+			{
+				for (W3CEndpointReference ref : r.getReturn()) {
+					System.out.println(ref.toString());
+				}	
+			}
+		} catch(ServerWebApplicationException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 
 }
