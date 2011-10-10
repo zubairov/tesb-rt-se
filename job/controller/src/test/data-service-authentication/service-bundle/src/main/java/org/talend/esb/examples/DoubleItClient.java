50e363886226eb3c0a097b6f76144a4829368e72
rminko <rnminko@gmail.com>
2011-10-10 11:33:27 +0300

    Implement delivering credentials from Data Service Consumer which uses user name / password

--- job/controller/src/test/data-service-authentication/service-bundle/src/main/java/org/talend/esb/examples/DoubleItClient.java
+++ job/controller/src/test/data-service-authentication/service-bundle/src/main/java/org/talend/esb/examples/DoubleItClient.java
@@ -1,14 +1,23 @@
 package org.talend.esb.examples;
 
+import javax.xml.ws.BindingProvider;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DoubleItClient {
 
 	public static void main(String[] args) throws Exception {
 
+		final String NAME = "karaf_name";
+		final String PASS = "karaf_password";
+		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"META-INF/spring/client-beans.xml");
 		DoubleIt client = (DoubleIt) context.getBean("DoubleItClient");
+
+		((BindingProvider)client).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, NAME);
+		((BindingProvider)client).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, PASS);
+
 		int result = client.execute(10);
 		
 		System.out.println("Result is " + result);
