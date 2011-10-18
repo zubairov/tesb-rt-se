437bb639668469d04af6374ba94af031e64dcf5c
amarkevich <amarkevich@talend.com>
2011-10-18 16:36:45 +0300

    [job] export policy provider as osgi service

---
+++ job/controller/src/main/java/org/talend/esb/job/controller/internal/PolicyProviderImpl.java
@@ -0,0 +1,70 @@
+/*
+ * #%L
+ * Talend :: ESB :: Job :: Controller
+ * %%
+ * Copyright (C) 2011 Talend Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.talend.esb.job.controller.internal;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.neethi.Policy;
+import org.apache.neethi.PolicyBuilder;
+import org.talend.esb.job.controller.PolicyProvider;
+
+public class PolicyProviderImpl implements PolicyProvider {
+
+    private String policyToken;
+
+    private String policySaml;
+
+    public void setPolicyToken(String policyToken) {
+        this.policyToken = policyToken;
+    }
+
+    public void setPolicySaml(String policySaml) {
+        this.policySaml = policySaml;
+    }
+
+    public Policy getTokenPolicy() {
+        return loadPolicy(policyToken);
+    }
+
+    public Policy getSamlPolicy() {
+        return loadPolicy(policySaml);
+    }
+
+    private static Policy loadPolicy(String location) {
+        InputStream is = null;
+        try {
+            is = new FileInputStream(location);
+            PolicyBuilder policyBuilder = new PolicyBuilder();
+            return policyBuilder.getPolicy(is);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot load policy");
+        } finally {
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    // just ignore
+                }
+            }
+        }
+    }
+}
