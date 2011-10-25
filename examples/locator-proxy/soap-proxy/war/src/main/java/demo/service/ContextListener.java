b934bf3b1ae6425976face64a63d15e102bcc7d5
Roman Minko <rnminko@gmail.com>
2011-10-25 11:58:44 +0300

    hot fix for proxy samples

--- examples/locator-proxy/soap-proxy/war/src/main/java/demo/service/ContextListener.java
+++ examples/locator-proxy/soap-proxy/war/src/main/java/demo/service/ContextListener.java
@@ -9,40 +9,44 @@ import org.talend.services.esb.locator.v1.ServiceLocatorFault;
 
 public class ContextListener implements ServletContextListener {
 
-	private ServletContext context = null;
+    private ServletContext context = null;
 
-	/*
-	 * This method is invoked when the Web Application has been removed and is
-	 * no longer able to accept requests
-	 */
+    /*
+     * This method is invoked when the Web Application has been removed and is
+     * no longer able to accept requests
+     */
 
-	public void contextDestroyed(ServletContextEvent event) {
-		// Output a simple message to the server's console
-		System.out.println("The Simple Web App. Has Been Removed");
-		this.context = null;
+    public void contextDestroyed(ServletContextEvent event) {
+        // Output a simple message to the server's console
+        System.out.println("The Simple Web App. Has Been Removed");
+        this.context = null;
 
-	}
+    }
 
-	// This method is invoked when the Web Application
-	// is ready to service requests
+    // This method is invoked when the Web Application
+    // is ready to service requests
 
-	public void contextInitialized(ServletContextEvent event) {
-		this.context = event.getServletContext();
+    public void contextInitialized(ServletContextEvent event) {
+        this.context = event.getServletContext();
 
-		// Output a simple message to the server's console
-		System.out.println("The Simple Web App. Is Ready");
+        // Output a simple message to the server's console
+        System.out.println("The Simple Web App. Is Ready");
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/client.xml");
-		LocatorService client = (LocatorService) context.getBean("locatorProxyService");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "/client.xml");
+        LocatorService client = (LocatorService) context
+                .getBean("locatorProxyService");
 
-		String serviceHost = this.context.getInitParameter("serviceHost");
+        String serviceHost = this.context.getInitParameter("serviceHost");
 
-		try {
-			client.registerEndpoint(new QName("http://talend.org/esb/examples/", "GreeterService"), serviceHost, null);
-		 } catch (InterruptedExceptionFault e) {
-			 e.printStackTrace();
-		 } catch (ServiceLocatorFault e) {
-			 e.printStackTrace();
-		 }
-	}
+        try {
+            client.registerEndpoint(new QName(
+                    "http://talend.org/esb/examples/", "GreeterService"),
+                    serviceHost, null, null, null);
+        } catch (InterruptedExceptionFault e) {
+            e.printStackTrace();
+        } catch (ServiceLocatorFault e) {
+            e.printStackTrace();
+        }
+    }
 }
