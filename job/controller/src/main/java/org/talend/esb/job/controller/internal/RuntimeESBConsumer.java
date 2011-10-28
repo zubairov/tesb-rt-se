b898c16404386fde85f2ac2f8b26522aa8808b38
amarkevich <amarkevich@talend.com>
2011-10-28 11:23:22 +0300

    [job] create client factory once

--- job/controller/src/main/java/org/talend/esb/job/controller/internal/RuntimeESBConsumer.java
+++ job/controller/src/main/java/org/talend/esb/job/controller/internal/RuntimeESBConsumer.java
@@ -38,6 +38,7 @@ import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.endpoint.EndpointException;
 import org.apache.cxf.feature.AbstractFeature;
+import org.apache.cxf.frontend.ClientFactoryBean;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.jaxws.JaxWsClientFactoryBean;
 import org.apache.cxf.service.Service;
@@ -67,16 +68,12 @@ public class RuntimeESBConsumer implements ESBConsumer {
     private static final String STS_SERVICE_NAME = "sts.service.name";
     private static final String STS_ENDPOINT_NAME = "sts.endpoint.name";
     private static final String CONSUMER_SIGNATURE_PASSWORD = "ws-security.signature.password";
-    
-    private final QName serviceName;
-    private final QName portName;
+
     private final String operationName;
-    private final String publishedEndpointUrl;
     private final boolean isRequestResponse;
-    private final LocatorFeature slFeature;
     private final EventFeature samFeature;
-    private final SecurityArguments securityArguments;
-    private final Bus bus;
+
+    private final ClientFactoryBean clientFactory;
 
     public RuntimeESBConsumer(final QName serviceName, 
             final QName portName,
@@ -87,33 +84,110 @@ public class RuntimeESBConsumer implements ESBConsumer {
             final EventFeature samFeature,
             final SecurityArguments securityArguments, 
             final Bus bus) {
-        this.serviceName = serviceName;
-        this.portName = portName;
         this.operationName = operationName;
-        this.publishedEndpointUrl = publishedEndpointUrl;
         this.isRequestResponse = isRequestResponse;
-        this.slFeature = slFeature;
         this.samFeature = samFeature;
-        this.securityArguments = securityArguments;
-        this.bus = bus;
+
+        clientFactory = new JaxWsClientFactoryBean() {
+            @Override
+            protected Endpoint createEndpoint() throws BusException,
+                    EndpointException {
+                final Endpoint endpoint = super.createEndpoint();
+                // set portType = serviceName
+                InterfaceInfo ii = endpoint.getService().getServiceInfos()
+                        .get(0).getInterface();
+                ii.setName(serviceName);
+                return endpoint;
+            }
+        };
+        clientFactory.setServiceName(serviceName);
+        clientFactory.setEndpointName(portName);
+        final String endpointUrl = (slFeature == null) ? publishedEndpointUrl
+                : "locator://" + serviceName.getLocalPart();
+        clientFactory.setAddress(endpointUrl);
+        clientFactory.setServiceClass(this.getClass());
+        clientFactory.setBus(bus);
+        final List<AbstractFeature> features = new ArrayList<AbstractFeature>();
+        if (slFeature != null) {
+            features.add(slFeature);
+        }
+        if (samFeature != null) {
+            features.add(samFeature);
+        }
+        if (null != securityArguments.getPolicy()) {
+            features.add(new WSPolicyFeature(securityArguments.getPolicy()));
+        }
+
+        clientFactory.setFeatures(features);
+
+        if (EsbSecurity.TOKEN == securityArguments.getEsbSecurity()) {
+            Map<String, Object> properties = new HashMap<String, Object>(2);
+            properties.put(SecurityConstants.USERNAME,
+                    securityArguments.getUsername());
+            properties.put(SecurityConstants.PASSWORD,
+                    securityArguments.getPassword());
+            clientFactory.setProperties(properties);
+        } else if (EsbSecurity.SAML == securityArguments.getEsbSecurity()) {
+            Map<String, String> sp = securityArguments.getStsProperties();
+
+            STSClient stsClient = new STSClient(bus);
+            stsClient.setWsdlLocation(sp.get(STS_WSDL_LOCATION));
+            stsClient.setServiceQName(new QName(sp.get(STS_NAMESPACE), sp
+                    .get(STS_SERVICE_NAME)));
+            stsClient.setEndpointQName(new QName(sp.get(STS_NAMESPACE), sp
+                    .get(STS_ENDPOINT_NAME)));
+
+            Map<String, Object> stsProperties = new HashMap<String, Object>();
+
+            for (Map.Entry<String, String> entry : sp.entrySet()) {
+                if (SecurityConstants.ALL_PROPERTIES.contains(entry.getKey())) {
+                    stsProperties.put(entry.getKey(), entry.getValue());
+                }
+            }
+
+            stsProperties.put(SecurityConstants.USERNAME,
+                    securityArguments.getUsername());
+            stsProperties.put(SecurityConstants.PASSWORD,
+                    securityArguments.getPassword());
+            stsClient.setProperties(stsProperties);
+
+            Map<String, Object> cfProperties = new HashMap<String, Object>();
+            cfProperties.put(SecurityConstants.STS_CLIENT, stsClient);
+
+            Map<String, String> sprop = securityArguments
+                    .getClientProperties();
+
+            for (Map.Entry<String, String> entry : sprop.entrySet()) {
+                if (SecurityConstants.ALL_PROPERTIES.contains(entry.getKey())) {
+                    cfProperties.put(entry.getKey(), entry.getValue());
+                }
+            }
+            cfProperties.put(SecurityConstants.CALLBACK_HANDLER,
+                    new WSPasswordCallbackHandler(
+                            sprop.get(SecurityConstants.SIGNATURE_USERNAME),
+                            sprop.get(CONSUMER_SIGNATURE_PASSWORD)));
+
+            clientFactory.setProperties(cfProperties);
+        }
+
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Object invoke(Object payload) throws Exception {
         if (payload instanceof org.dom4j.Document) {
             return sendDocument((org.dom4j.Document) payload);
-        } else if (payload instanceof java.util.Map<?,?>) {
-            @SuppressWarnings("unchecked")
-            Map<String, Object> map = (Map<String, Object>) payload;
+        } else if (payload instanceof java.util.Map) {
+            Map<?, ?> map = (Map<?, ?>) payload;
 
-            @SuppressWarnings("unchecked")
-            Map<String, String> samProps = (Map<String, String>) map
-                    .get(ESBProviderBase.REQUEST_SAM_PROPS);
-            if (samProps != null && samFeature != null) {
-                LOG.info("SAM custom properties received: " + samProps);
-                CustomInfoHandler ciHandler = new CustomInfoHandler();
-                ciHandler.setCustomInfo(samProps);
-                samFeature.setHandler(ciHandler);
+            if (samFeature != null) {
+                Object samProps = map.get(ESBProviderBase.REQUEST_SAM_PROPS);
+                if (samProps != null) {
+                    LOG.info("SAM custom properties received: " + samProps);
+                    CustomInfoHandler ciHandler = new CustomInfoHandler();
+                    ciHandler.setCustomInfo((Map<String, String>)samProps);
+                    samFeature.setHandler(ciHandler);
+                }
             }
 
             return sendDocument((org.dom4j.Document) map
@@ -152,91 +226,7 @@ public class RuntimeESBConsumer implements ESBConsumer {
     }
 
     private Client createClient() throws BusException, EndpointException {
-        final JaxWsClientFactoryBean cf = new JaxWsClientFactoryBean() {
-            @Override
-            protected Endpoint createEndpoint() throws BusException,
-                    EndpointException {
-                final Endpoint endpoint = super.createEndpoint();
-                // set portType = serviceName
-                InterfaceInfo ii = endpoint.getService().getServiceInfos()
-                        .get(0).getInterface();
-                ii.setName(serviceName);
-                return endpoint;
-            }
-        };
-        cf.setServiceName(serviceName);
-        cf.setEndpointName(portName);
-        final String endpointUrl = (slFeature == null) ? publishedEndpointUrl
-                : "locator://" + serviceName.getLocalPart();
-        cf.setAddress(endpointUrl);
-        cf.setServiceClass(this.getClass());
-        cf.setBus(bus);
-        final List<AbstractFeature> features = new ArrayList<AbstractFeature>();
-        if (slFeature != null) {
-            features.add(slFeature);
-        }
-        if (samFeature != null) {
-            features.add(samFeature);
-        }
-        if (null != securityArguments.getPolicy()) {
-            features.add(new WSPolicyFeature(securityArguments.getPolicy()));
-        }
-
-        cf.setFeatures(features);
-
-        if (EsbSecurity.NO != securityArguments.getEsbSecurity()) {
-            if (EsbSecurity.TOKEN == securityArguments.getEsbSecurity()) {
-                Map<String, Object> properties = new HashMap<String, Object>(2);
-                properties.put(SecurityConstants.USERNAME,
-                        securityArguments.getUsername());
-                properties.put(SecurityConstants.PASSWORD,
-                        securityArguments.getPassword());
-                cf.setProperties(properties);
-            } else if (EsbSecurity.SAML == securityArguments.getEsbSecurity()) {
-                Map<String, String> sp = securityArguments.getStsProperties();
-
-                STSClient stsClient = new STSClient(bus);
-                stsClient.setWsdlLocation(sp.get(STS_WSDL_LOCATION));
-                stsClient.setServiceQName(new QName(sp.get(STS_NAMESPACE), sp
-                        .get(STS_SERVICE_NAME)));
-                stsClient.setEndpointQName(new QName(sp.get(STS_NAMESPACE), sp
-                        .get(STS_ENDPOINT_NAME)));
-
-                Map<String, Object> stsProperties = new HashMap<String, Object>();
-
-                for (Map.Entry<String, String> entry : sp.entrySet()) {
-                    if (SecurityConstants.ALL_PROPERTIES.contains(entry.getKey())) {
-                        stsProperties.put(entry.getKey(), entry.getValue());
-                    }
-                }
-
-                stsProperties.put(SecurityConstants.USERNAME,
-                        securityArguments.getUsername());
-                stsProperties.put(SecurityConstants.PASSWORD,
-                        securityArguments.getPassword());
-                stsClient.setProperties(stsProperties);
-
-                Map<String, Object> cfProperties = new HashMap<String, Object>();
-                cfProperties.put(SecurityConstants.STS_CLIENT, stsClient);
-
-                Map<String, String> sprop = securityArguments
-                        .getClientProperties();
-
-                for (Map.Entry<String, String> entry : sprop.entrySet()) {
-                    if (SecurityConstants.ALL_PROPERTIES.contains(entry.getKey())) {
-                        cfProperties.put(entry.getKey(), entry.getValue());
-                    }
-                }
-                cfProperties.put(SecurityConstants.CALLBACK_HANDLER,
-                        new WSPasswordCallbackHandler(
-                                sprop.get(SecurityConstants.SIGNATURE_USERNAME),
-                                sprop.get(CONSUMER_SIGNATURE_PASSWORD)));
-
-                cf.setProperties(cfProperties);
-            }
-        }
-
-        Client client = cf.create();
+        Client client = clientFactory.create();
 
         final Service service = client.getEndpoint().getService();
         service.setDataBinding(new SourceDataBinding());
