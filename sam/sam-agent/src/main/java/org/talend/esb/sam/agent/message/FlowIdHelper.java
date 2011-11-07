36d9395fc55d31acf3a732717a80b35477e0c9b3
Vladimir Romaniuk <vromaniuk@talend.com>
2011-11-07 19:50:17 +0200

    SAM fix for flowid rendering

--- sam/sam-agent/src/main/java/org/talend/esb/sam/agent/message/FlowIdHelper.java
+++ sam/sam-agent/src/main/java/org/talend/esb/sam/agent/message/FlowIdHelper.java
@@ -42,9 +42,13 @@ public class FlowIdHelper {
 	public static String getFlowId(Message message) {
 		String flowId = (String) message.get(FLOW_ID_KEY);
 		if (null == flowId) {
-			Map<String, ArrayList<String>> mesHeaders = (TreeMap) message
-					.get(Message.PROTOCOL_HEADERS);
-			flowId = (null == mesHeaders) ? null : mesHeaders.get(FLOW_ID_KEY).toString();
+			Object headers = message.get(Message.PROTOCOL_HEADERS);
+			if (headers instanceof TreeMap<?, ?>) {
+				Map<String, ArrayList<String>> mesHeaders = (TreeMap<String, ArrayList<String>>) message
+						.get(Message.PROTOCOL_HEADERS);
+				flowId = (null == mesHeaders) ? null : mesHeaders.get(
+						FLOW_ID_KEY).toString();
+			}
 		}
 		return flowId;
 	}
