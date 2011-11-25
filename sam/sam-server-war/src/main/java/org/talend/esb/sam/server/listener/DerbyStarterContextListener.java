3e2880e0bd1ff0aa28f0a316396d055f9d0a3270
amarkevich <amarkevich@talend.com>
2011-11-25 16:37:59 +0200

    fixed style

--- sam/sam-server-war/src/main/java/org/talend/esb/sam/server/listener/DerbyStarterContextListener.java
+++ sam/sam-server-war/src/main/java/org/talend/esb/sam/server/listener/DerbyStarterContextListener.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * Service Activity Monitoring :: Server
+ * %%
+ * Copyright (C) 2011 Talend Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package org.talend.esb.sam.server.listener;
 
 import java.net.InetAddress;
@@ -8,37 +27,33 @@ import javax.servlet.ServletContextListener;
 import org.apache.derby.drda.NetworkServerControl;
 
 public class DerbyStarterContextListener implements ServletContextListener {
-	private NetworkServerControl server;
-	private boolean startDerby;
 
-	public DerbyStarterContextListener() {
-		startDerby = false;
-		String startDerbyProperty = System.getProperty("org.talend.esb.sam.server.embedded");
-		if ((startDerbyProperty != null)
-				&& (startDerbyProperty.toUpperCase().equals("TRUE"))) {
-			startDerby = true;
-		}
-	}
+    private final boolean startDerby;
+    private NetworkServerControl server;
 
-	public void contextDestroyed(ServletContextEvent arg0) {
-		if (startDerby) {
-			try {
-				server.shutdown();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public DerbyStarterContextListener() {
+        startDerby = "TRUE".equalsIgnoreCase(System.getProperty("org.talend.esb.sam.server.embedded"));
+    }
 
-	public void contextInitialized(ServletContextEvent arg0) {
-		if (startDerby) {
-			try {
-				server = new NetworkServerControl(
-						InetAddress.getByName("localhost"), 1527);
-				server.start(null);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public void contextDestroyed(ServletContextEvent arg0) {
+        if (startDerby) {
+            try {
+                server.shutdown();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void contextInitialized(ServletContextEvent arg0) {
+        if (startDerby) {
+            try {
+                server = new NetworkServerControl(
+                        InetAddress.getByName("localhost"), 1527);
+                server.start(null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
